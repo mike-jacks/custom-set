773f9cb13d0d327f10c60aaad89c7afa62f8b819
Mike Jacks <mjacks4816@xmail.dixietech.edu>
2024-02-09 09:19:00 -0700

    modify self.custom_set field in CustomSet class to be self.set and ripple change throughout class methods

--- custom_set.py
+++ custom_set.py
@@ -1,24 +1,24 @@
 class CustomSet:
     def __init__(self) -> None:
-        self.custom_set: list[object] = []
+        self.set: list[object] = []
 
     def add(self, item: object) -> None:
-        if item not in self.custom_set:
-            self.custom_set.append(item)
+        if item not in self.set:
+            self.set.append(item)
         else:
             print(f"{item} is already in the set")
 
     def remove(self, item: object) -> None:
-        if item in self.custom_set:
-            self.custom_set.remove(item)
+        if item in self.set:
+            self.set.remove(item)
         else:
             print(f"{item} is not in the set")
 
     def as_list(self) -> list[object]:
-        return self.custom_set
+        return self.set
 
     def clear(self) -> None:
-        self.custom_set = []
+        self.set = []
 
 def main():
     my_set = CustomSet()
