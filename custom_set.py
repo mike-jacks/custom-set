ba5660c7004ca3eb2048a75cabea0673d5c9587f
Mike Jacks <mjacks4816@xmail.dixietech.edu>
2024-02-09 08:47:15 -0700

    add class CustomSet with empty methods

---
+++ custom_set.py
@@ -0,0 +1,12 @@
+class CustomSet:
+    def __init__(self):
+        pass
+    def add(self, item: object) -> None:
+        pass
+    def remove(self, item: object) -> None:
+        pass
+    def as_list(self) -> list[object]:
+        pass
+    def clear(self) -> None:
+        pass
+
